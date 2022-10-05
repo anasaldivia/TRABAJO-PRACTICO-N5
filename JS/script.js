24558d9f206449ffe87ecdf93d640bf94487e147
Ana Saldivia <vickyanasol12@gmail.com>
2022-10-05 15:42:46 -0300

    Detallando

--- JS/script.js
+++ JS/script.js
@@ -5,21 +5,21 @@ const edad = 16;
 var num1 = 500;
 var num2 = 10;
 //#endregion
+
 //#region 
 console.log(nombre)
 console.log(apellido)
 console.log(edad)
 //#endregion
+
 //#region 
 let x = num1/num2;
 console.log(x);
 //#endregion
+
 //#region
 let numeros=[10, 20];
 for (numeros of numeros){
     console.log(numeros);
 }
-//#endregion
-
-
-
+//#endregion 
